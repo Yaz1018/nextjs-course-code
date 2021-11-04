6a0f948a1dfd749bb6cf1c3ab1c90cc1327e4169
Yaz1018 <garoncdavis@gmail.com>
2021-11-03 21:44:59 -0400

    meetups happening

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+
+import AllMeetups from "./pages/AllMeetups";
+import NewMeetups from "./pages/NewMeetups";
+import Favorites from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <MainNavigation></MainNavigation>
+      <Routes>
+        <Route path='/' element={<AllMeetups />} />
+        <Route path='new-meetups' element={<NewMeetups />} />
+        <Route path='favorites' element={<Favorites />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/NewMeetups.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NewMeetups() {
+  return <div>New Meet up page</div>;
+}
+
+export default NewMeetups;
